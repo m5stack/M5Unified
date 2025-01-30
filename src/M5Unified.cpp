cf9480e4a5f0f7a351dc6fb8f7c841713b131f0e
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2025-01-30 09:57:51 +0900

    fix complier warning

--- src/M5Unified.cpp
+++ src/M5Unified.cpp
@@ -334,7 +334,6 @@ static constexpr const uint8_t _pin_table_other1[][2] = {
   {
     (void)args;
     (void)enabled;
-    auto self = (M5Unified*)args;
     static constexpr const uint8_t enabled_bulk_data[] = {
       2, 0x00, 0x00,  // 0x00 RESET/  CSM POWER DOWN
       2, 0x00, 0x80,  // 0x00 RESET/  CSM POWER ON
@@ -436,7 +435,6 @@ static constexpr const uint8_t _pin_table_other1[][2] = {
   {
     (void)args;
     (void)enabled;
-    auto self = (M5Unified*)args;
     static constexpr const uint8_t enabled_bulk_data[] = {
       2, 0x00, 0x00,  // 0x00 RESET/  CSM POWER DOWN
       2, 0x00, 0x80,  // 0x00 RESET/  CSM POWER ON
