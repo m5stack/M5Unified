cf82fb4ee986445671c178ffdd1802f07c69568b
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2025-10-22 17:28:34 +0900

    fix compile error

--- src/utility/led/LED_PowerHub.cpp
+++ src/utility/led/LED_PowerHub.cpp
@@ -2,8 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include "LED_PowerHub.hpp"
-#include "esp_log.h"
-#include "Arduino.h"
 
 namespace m5
 {
