bdad52ecb4ca6488861039675a94f1df233727d8
Tinyu <tinyu0806@gmail.com>
2026-07-17 15:34:26 +0800

    Update PaperMono LED IOE1 control

--- src/utility/led/LED_PaperMono_Class.cpp
+++ src/utility/led/LED_PaperMono_Class.cpp
@@ -3,6 +3,7 @@
 
 #include "LED_PaperMono_Class.hpp"
 #include "../../M5Unified.hpp"
+#include "../M5IOE1_Class.hpp"
 
 #if defined (CONFIG_IDF_TARGET_ESP32S3)
 
@@ -10,35 +11,35 @@ namespace m5
 {
 // RGBLED_R = PMIC -> LED_EN_PP
 // RGBLED_G = PYB -> G8
-// RGBLED_B = PYB -> G2
+// RGBLED_B = PYB -> G9
 
   static constexpr size_t led_count = 1;
   static constexpr uint8_t m5pm1_i2c_addr = 0x6E;
-  static constexpr uint8_t m5ioe1_i2c_addr = 0x4F;
   static constexpr uint32_t i2c_freq = 100000;
+  static constexpr auto ioe1_led_g_pin = M5IOE1_Class::gpio8;
+  static constexpr auto ioe1_led_b_pin = M5IOE1_Class::gpio9;
 
   bool LED_PaperMono_Class::begin(void)
   {
     // PM1 LED_EN (red), set PP mode and enable LED output
     M5.In_I2C.bitOff(m5pm1_i2c_addr, 0x13, 0x20, i2c_freq);
 
-    // IOE1 IO2 (blue), IO8 (green) output
-    M5.In_I2C.bitOn(m5ioe1_i2c_addr, 0x03, 0x82, i2c_freq);
+    auto& ioe1 = static_cast<M5IOE1_Class&>(M5.getIOExpander(0));
+    ioe1.setDirection(ioe1_led_g_pin, true);
+    ioe1.setDirection(ioe1_led_b_pin, true);
+    ioe1.setHighImpedance(ioe1_led_g_pin, false);
+    ioe1.setHighImpedance(ioe1_led_b_pin, false);
 
-    // IOE1 IO2 (blue), IO8 (green) push-pull
-    M5.In_I2C.bitOff(m5ioe1_i2c_addr, 0x13, 0x82, i2c_freq);
-
-
-    {
-      setBrightness(_brightness);
-      return true;
-    }
-    return false;
+    setBrightness(_brightness);
+    return true;
   }
 
   void LED_PaperMono_Class::setColors(const RGBColor* values, size_t index, size_t length)
   {
-    if (index + length > led_count) {
+    if (index >= led_count) {
+      return;
+    }
+    if (length > led_count - index) {
       length = led_count - index;
     }
     std::copy(values, values + length, &_rgb_buffer + index);
@@ -47,16 +48,13 @@ namespace m5
   void LED_PaperMono_Class::setBrightness(const uint8_t brightness)
   {
     _brightness = brightness;
-    std::array<uint8_t, led_count> br_buffer;
-    br_buffer.fill(brightness);
-    // writeRegister(0x80, br_buffer.data(), br_buffer.size());
   }
 
   void LED_PaperMono_Class::display(void)
   {
     // RED = PMIC -> LED_EN_PP
     // GREEN = PYB -> G8
-    // BLUE = PYB -> G2 
+    // BLUE = PYB -> G9
     uint32_t br = _brightness + 1;
     br = br * br;
 
@@ -73,23 +71,12 @@ namespace m5
       M5.In_I2C.bitOn(m5pm1_i2c_addr, 0x06, 0x10, i2c_freq);
     }
 
-    if (g < 2048) {
-      M5.In_I2C.bitOff(m5ioe1_i2c_addr, 0x05, 0x80, i2c_freq);
-    } else {
-      M5.In_I2C.bitOn(m5ioe1_i2c_addr, 0x05, 0x80, i2c_freq);
-    }
+    auto& ioe1 = static_cast<M5IOE1_Class&>(M5.getIOExpander(0));
+    ioe1.digitalWrite(ioe1_led_g_pin, g >= 2048);
+    ioe1.digitalWrite(ioe1_led_b_pin, b >= 2048);
 
-    if (b < 2048) {
-      M5.In_I2C.bitOff(m5ioe1_i2c_addr, 0x05, 0x02, i2c_freq);
-    } else {
-      M5.In_I2C.bitOn(m5ioe1_i2c_addr, 0x05, 0x02, i2c_freq);
-    }
-
-    { // PWM2 (IO8) for green
-      if (g > 4095) g = 4095;
-      uint8_t data[2] = { uint8_t(g & 0xFF), uint8_t((g >> 8) | 0x80) };
-      M5.In_I2C.writeRegister(m5ioe1_i2c_addr, 0x1D, data, sizeof(data), i2c_freq);
-    }
+    if (g > 4095) { g = 4095; }
+    ioe1.setPwmDuty(M5IOE1_Class::pwm_ch2, g, g > 0);
   }
 }
 
